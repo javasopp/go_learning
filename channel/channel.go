73e1450a84e29e743f00a8fec12997af14cb329c
lizhiping <893891491@qq.com>
2022-06-02 16:20:49 +0800

    submitted by sopp for : channel 的基本使用，下一章csp模型，基于goroutine使用。

--- channel/channel.go
+++ channel/channel.go
@@ -5,23 +5,66 @@ import (
 	"time"
 )
 
-func workder(id int, c chan int) {
-	for {
-		fmt.Printf("Worker %d received %d\n", id, <-c)
-	}
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("Worker %d received %c\n", id, <-c)
+		}
+	}()
+	return c
 }
 
 func chanDemo() {
-	c := make(chan int)
-	go workder(1, c)
-	c <- 1
-	c <- 2
-	c <- 3
-	c <- 4
+	var channels [10]chan<- int
+	for i := 0; i < 10; i++ {
+		channels[i] = createWorker(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
+
 	time.Sleep(time.Millisecond)
 	//var c chan int // c == nil
 }
 
 func main() {
-	chanDemo()
+	//chanDemo()
+
+	//bufferedChannel()
+	closeBufferedChannel()
+}
+
+func worker(id int, c chan int) {
+	for n := range c {
+		//n, ok := <-c
+		//if !ok {
+		//	break
+		//}
+		fmt.Printf("Worker %d received %c\n", id, n)
+	}
+}
+
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	time.Sleep(time.Millisecond)
+}
+
+func closeBufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	close(c)
+	time.Sleep(time.Millisecond)
 }
